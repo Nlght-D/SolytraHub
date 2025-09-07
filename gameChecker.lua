28e53acea52d8308b5386d1ad2c5f8708011c9f8
Nlght-D <caiomtmurilo041@gmail.com>
2025-09-07 17:17:58 -0300

    script

---
+++ gameChecker.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/main/Library.lua"))()
+local Window = Library:CreateWindow({
+    Title = "My Script",
+    Footer = "",
+    ToggleKeybind = Enum.KeyCode.RightControl,
+    Center = true,
+    AutoShow = true
+})
+
