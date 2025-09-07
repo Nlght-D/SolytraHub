3dd3d4237a7660d2a27aa3adf07f582d877ab7bc
Nlght-D <caiomtmurilo041@gmail.com>
2025-09-07 19:58:39 -0300

    KSPR

--- Games/KSPR.lua
+++ Games/KSPR.lua
@@ -14,3 +14,19 @@ local GloveGroupBox = MainTab:AddLeftGroupbox("Glove Stuffs")
 local TabGlove = MainTab:AddLeftTabbox("Glove Stuffs")
 local SlapGlove = GloveTab:AddTab("Slap")
 local GenralGlove = GloveTab:AddTab("General")
+
+local Button = SlapGlove:AddButton({
+    Text = "Click Me",
+    Func = function()
+        print("Button clicked!")
+    end,
+    DoubleClick = true -- Requires double-click for risky actions
+})
+
+-- You can also add sub-buttons
+Button:AddButton({
+    Text = "Sub Button",
+    Func = function()
+        print("Sub-button clicked!")
+    end
+})
