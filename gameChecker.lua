cdfd0166f61f9ec64e38d512a5735f3ef10a485e
Nlght-D <caiomtmurilo041@gmail.com>
2025-09-07 18:50:57 -0300

    script

--- gameChecker.lua
+++ gameChecker.lua
@@ -1,20 +1,28 @@
 local Places = {
-    102719677723924 = "https://raw.githubusercontent.com/Nlght-D/SolytraHub/refs/heads/main/Games/KSPE.lua"
+    [102719677723924] = "https://raw.githubusercontent.com/Nlght-D/SolytraHub/refs/heads/main/Games/KSPE.lua"
 }
-print("loaded")
+
 local PlaceId = game.PlaceId
 
 if Places[PlaceId] then
     if loadstring then
         local success, err = pcall(function()
-            loadstring(game:HttpGet(Places[PlaceId]))()
+            local scriptContent = game:HttpGet(Places[PlaceId])
+            local func = loadstring(scriptContent)
+            if func then
+                func()
+            else
+                warn("Erro ao compilar o script.")
+            end
         end)
         if not success then
             warn("Erro ao executar o script:", err)
         else
-            print("Loaded")
+            print("Script carregado e executado com sucesso.")
         end
     else
-        warn("loadstring não está disponível neste ambiente")
+        warn("loadstring não está disponível neste ambiente.")
     end
+else
+    warn("ID do lugar não encontrado na tabela.")
 end
