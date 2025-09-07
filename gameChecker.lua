b647275c3a359e7eae6b26f0c78c20dbe24ab8b5
Nlght-D <caiomtmurilo041@gmail.com>
2025-09-07 17:31:46 -0300

    script

--- gameChecker.lua
+++ gameChecker.lua
@@ -1,6 +1,8 @@
 local Places = {
     102719677723924 = "https://raw.githubusercontent.com/Nlght-D/SolytraHub/refs/heads/main/Games/KSPE.lua?token=GHSAT0AAAAAADJJLAFD4GL27MRNMJSX73NM2F55JGQ"
 }
-local PlaceId = game.PlaceId if Places[PlaceId] then
+local PlaceId = game.PlaceId 
+if Places[PlaceId] then
+    print("tentando carregar")
     loadstring(game:HttpGet(Places[PlaceId]))
 end
