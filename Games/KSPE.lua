70e419fdf887e69729059f6b35b18717118b8923
Nlght-D <caiomtmurilo041@gmail.com>
2025-09-07 18:03:24 -0300

    script

--- Games/KSPE.lua
+++ Games/KSPE.lua
@@ -1,8 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/main/Library.lua"))()
 local Window = Library:CreateWindow({
     Title = "KS:PR,
-    Footer = "",
+    Footer = "Made by: #?%#(*",
     ToggleKeybind = Enum.KeyCode.RightControl,
     Center = true,
     AutoShow = true
-})
+}) local MainTab = Window:AddTab("Main", "home")
