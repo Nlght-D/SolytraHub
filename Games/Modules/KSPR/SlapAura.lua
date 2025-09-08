cb559818d314496d1387576be28952d696fb06e4
Nlght-D <caiomtmurilo041@gmail.com>
2025-09-08 18:12:30 -0300

    KSPR

--- Games/Modules/KSPR/SlapAura.lua
+++ Games/Modules/KSPR/SlapAura.lua
@@ -1,57 +1,57 @@
 local SlapAura = {}
-local player = game:GetService("Players").LocalPlayer
+local plr = game:GetService("Players").LocalPlayer
 
 function SlapAura.Setup()
     while _G.SlapAuraActivated do
         local char = plr.Character or plr.CharacterAdded:Wait()
         local tool = char:FindFirstChild("Soul Devourer") or plr.Backpack:FindFirstChild("Soul Devourer")
+        
         if not tool then
-            tool = plr.Backpack.ChildAdded:Wait()
-            while tool.Name ~= "Soul Devourer" do
-                tool = char.ChildAdded:Wait()
-                task.wait()
-            end
+            repeat
+                tool = plr.Backpack.ChildAdded:Wait()
+            until tool.Name == "Soul Devourer"
         end
+        
         local SlapEvent = tool:WaitForChild("Server"):WaitForChild("Slap")
         local range = tonumber(_G.SlapAuraArea) or 10
-        task.spawn(function()
+
+        local charHRP = char:FindFirstChild("HumanoidRootPart") 
+                        or char:FindFirstChild("Torso") 
+                        or char:FindFirstChild("UpperTorso")
+        if charHRP then
             for _, v in ipairs(game:GetService("Players"):GetPlayers()) do
                 if v ~= plr and v.Character then
                     local hum = v.Character:FindFirstChildOfClass("Humanoid")
                     local hrp = v.Character:FindFirstChild("HumanoidRootPart")
-                        or v.Character:FindFirstChild("Torso")
-                        or v.Character:FindFirstChild("UpperTorso")
-
+                            or v.Character:FindFirstChild("Torso")
+                            or v.Character:FindFirstChild("UpperTorso")
                     if hum and hrp and hum.Health > 0 then
-                        if (hrp.Position - char.HumanoidRootPart.Position).Magnitude <= range then
+                        if (hrp.Position - charHRP.Position).Magnitude <= range then
                             SlapEvent:FireServer(v.Character)
                         end
                     end
                 end
             end
-        end)
 
-        task.spawn(function()
-            
             for _, v in ipairs(workspace:WaitForChild("Dummies"):GetChildren()) do
                 if v:IsA("Model") then
                     local hum = v:FindFirstChildOfClass("Humanoid")
                     local hrp = v:FindFirstChild("HumanoidRootPart")
                             or v:FindFirstChild("Torso")
                             or v:FindFirstChild("UpperTorso")
-
                     if hum and hrp and hum.Health > 0 then
-                        if (hrp.Position - char.HumanoidRootPart.Position).Magnitude <= range then
+                        if (hrp.Position - charHRP.Position).Magnitude <= range then
                             SlapEvent:FireServer(v)
-                           end
+                        end
                     end
                 end
             end
-        end)
-        
-        task.wait(.1)
+        end
+
+        task.wait(0.1)
     end
 end
+
 
 function SlapAura.Setup(b)
     print("Loaded")
