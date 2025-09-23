1ca8e7c8b66b41fdaa3c267095b095dbbec20e6b
Nlght-D <caiomtmurilo041@gmail.com>
2025-09-22 22:19:21 -0300

    Ink Gmae

--- Games/InkGame.lua
+++ Games/InkGame.lua
@@ -2,12 +2,13 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deivi
 local Window = Library:CreateWindow({
     Title = "Ink Game",
     Footer = "Solytra Hub",
+    Icon = "triangle",
     ToggleKeybind = Enum.KeyCode.M,
     Center = true,
     AutoShow = true
 }) 
 
-local GamesTab = Window:AddTab("Games", "home")
+local GamesTab = Window:AddTab("Games", "gamepad")
 local CombatTab = Window:AddTab("Combat", "crosshair")
 
-local GLRDTab = MainTab:AddLeftGroupbox("Green Light Red Light")
+local GLRDTab = GamesTab:AddLeftGroupbox("Green Light Red Light")
