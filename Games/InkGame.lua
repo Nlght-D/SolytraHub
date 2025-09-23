e3e1e37c27f392c54e6f6dbc739e5116dc44bb6f
Nlght-D <caiomtmurilo041@gmail.com>
2025-09-22 21:05:34 -0300

    Ink game

---
+++ Games/InkGame.lua
@@ -0,0 +1,8 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/main/Library.lua"))()
+local Window = Library:CreateWindow({
+    Title = "Ink Game",
+    Footer = "Solytra Hub",
+    ToggleKeybind = Enum.KeyCode.M,
+    Center = true,
+    AutoShow = true
+}) 
